9431abb0b582a4a15707051b7f75fe7e06eb4c6c
latronixo1 <latronixo@gmail.com>
2023-03-01 09:22:26 +0300

    Закончил видео 144

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,49 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchTextField.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    //нажатие кнопки с изображением лупы
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!) 
+    }
+    
+    //функция делегата (в названии есть Should"), которая вызывается в момент нажатия кнопки возврата (в данный момент это кнопка "Go" на экранной клавиатуре)
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    //функция делегата (в названии есть Should") - окончание ввода текст в текстовое поле
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        //если в текстовом поле остался какой-то текст, то возвращаем true
+        if textField.text == "" {
+            return true
+        } else {
+            //а если текстовое поле пустое, то вернем false
+            textField.placeholder = "Введите что-нибудь"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //используйте searchTextField для поиска погоды для этого города
+        
+        searchTextField.text = ""   //очищаем строку поиска после нажатия кнопки "Go" на экранной клавиатуре или кнопки с изображением лупы
+    }
 }
 
